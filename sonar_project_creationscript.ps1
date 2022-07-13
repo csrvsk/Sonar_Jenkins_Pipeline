a87bd6ab6734347e6fb39847480ed564d6dbc61d
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-13 12:14:45 -0400

    Update sonar_project_creationscript.ps1

--- sonar_project_creationscript.ps1
+++ sonar_project_creationscript.ps1
@@ -9,6 +9,7 @@ sonar.login=c1566905ddf4a26f5db614f4209262ed73367afb
 sonar.sources=. 
 sonar.java.binaries=.
 sonar.inclusions=/$folder/**
+sonar.exclusions=Jenkins-sonar.groovy,sonar_project_creationscript.ps1,README.md,sonar-project.properties
 "@
 Set-Content ./sonar-project.properties $contentToAdd
 $content = Get-Content sonar-project.properties

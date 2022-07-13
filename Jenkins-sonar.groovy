9a0283fb742a363dd3a4190c322e4e46d69f8d9a
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-13 12:15:53 -0400

    Update Jenkins-sonar.groovy

--- Jenkins-sonar.groovy
+++ Jenkins-sonar.groovy
@@ -6,7 +6,7 @@ pipeline {
 		stage("Sonar"){
 			steps{
 				script{
-					powershell(returnStatus: true, script: 'sonar_project_creationscript.ps1') 
+					//powershell(returnStatus: true, script: 'sonar_project_creationscript.ps1') 
 				}
 			}
 		}

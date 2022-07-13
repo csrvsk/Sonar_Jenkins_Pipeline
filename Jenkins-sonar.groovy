3ab40acc45710a4c7928533437476271874efa69
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-13 12:27:01 -0400

    Update Jenkins-sonar.groovy

--- Jenkins-sonar.groovy
+++ Jenkins-sonar.groovy
@@ -6,7 +6,7 @@ pipeline {
 		stage("checkout"){
 			steps{
 				script{
-					powershell 'svn checkout https://cmicsvn.cmic.ca/erp/R12/Development/Trunk/V12/AP --username ShivaKumar.Vardhamaan@cmic.ca --password Shanmukh@2020'
+					powershell 'svn checkout https://cmicsvn.cmic.ca/erp/R12/Development/Trunk/V12/AP . --username ShivaKumar.Vardhamaan@cmic.ca --password Shanmukh@2020'
 					//powershell(returnStatus: true, script: 'sonar_project_creationscript.ps1') 
 				}
 			}

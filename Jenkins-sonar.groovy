7ac57653af9fd8c0ca64c63b69692bb9663344fd
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-13 11:18:37 -0400

    Create Jenkins-sonar.groovy

---
+++ Jenkins-sonar.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent {
+	label "Node_machine"
+}
+	stages {
+		stage("Sonar"){
+			steps{
+				script{
+					powershell(returnStatus: true, script: 'sonar_project_creationscript.ps1') 
+				}
+			}
+		}
+	}
+}

61b16e40bffb74a674b87789673605c89d072f9b
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-14 12:30:31 -0400

    Update Jenkins-sonar.groovy

--- Jenkins-sonar.groovy
+++ Jenkins-sonar.groovy
@@ -20,5 +20,18 @@ pipeline {
 				}
 			}
 		}
+		stage("cleaning workspace"){
+			steps{
+				script{
+					powershell 'Get-Childitem -Include .scannerwork -Recurse -force | Remove-Item -Force -Recurse'
+					powershell 'Get-Childitem -Include .svn -Recurse -force | Remove-Item -Force -Recurse'
+					powershell 'Get-Childitem -Include .git -Recurse -force | Remove-Item -Force -Recurse'
+					powershell 'Get-Childitem -Include sonar-project.properties -Recurse -force | Remove-Item -Force -Recurse'
+					powershell 'Get-Childitem -Include sonar_project_creationscript.ps1 -Recurse -force | Remove-Item -Force -Recurse'
+					powershell 'Get-Childitem -Include Jenkins-sonar.groovy -Recurse -force | Remove-Item -Force -Recurse'
+					//powershell './sonar_project_creationscript.ps1' 
+				}
+			}
+		}
 	}
 }

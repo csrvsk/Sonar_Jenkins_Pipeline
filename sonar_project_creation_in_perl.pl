f49cc8f8ed1be95f855fd9cc3e3c47b0ce352e5e
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-15 13:58:47 -0400

    Update sonar_project_creation_in_perl.pl

--- sonar_project_creation_in_perl.pl
+++ sonar_project_creation_in_perl.pl
@@ -9,7 +9,7 @@ opendir my $dh, $path
   or die "$0: opendir: $!";
 
 my @dirs = grep {-d "$path/$_" && ! /^\.{1,2}$/} readdir($dh);
-my $name="V12_";
+my $name="V12_AP_";
 foreach ( @dirs ) {
 	$proname=$name.$_;
 	my $file = "sonar-project.properties";
@@ -19,3 +19,6 @@ foreach ( @dirs ) {
 	print FILE "sonar.projectKey=$proname\nsonar.projectName=$proname\nsonar.login=c1566905ddf4a26f5db614f4209262ed73367afb\nsonar.sources=.\nsonar.java.binaries=.\nsonar.inclusions=/$_/**\nsonar.exclusions=Jenkins-sonar.groovy,sonar_project_creationscript.ps1,sonar_project_creation_in_perl.pl,README.md,sonar-project.properties\n";
 	close FILE;
 	}
+my $powershell = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe';
+my $commandin = 'dir';
+$result = `$powershell -command "$commandin"`;

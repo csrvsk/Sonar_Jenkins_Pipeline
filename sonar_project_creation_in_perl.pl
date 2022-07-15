7545a2a567ad93495118f1a97476609c49db755f
csrvsk <70667412+csrvsk@users.noreply.github.com>
2022-07-15 14:33:46 -0400

    Update sonar_project_creation_in_perl.pl

--- sonar_project_creation_in_perl.pl
+++ sonar_project_creation_in_perl.pl
@@ -18,8 +18,6 @@ foreach ( @dirs ) {
 	}
 	print FILE "sonar.projectKey=$proname\nsonar.projectName=$proname\nsonar.login=c1566905ddf4a26f5db614f4209262ed73367afb\nsonar.sources=.\nsonar.java.binaries=.\nsonar.inclusions=/$_/**\nsonar.exclusions=Jenkins-sonar.groovy,sonar_project_creationscript.ps1,sonar_project_creation_in_perl.pl,README.md,sonar-project.properties\n";
 	close FILE;
-	my $powershell = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe';
 	my $commandin = 'sonar-scanner';
-	my $result = `$powershell -command "$commandin"`;
-	print "$result"
+	'$commandin';
 }
